17a8d1c5433852aa3fbc1e120ede76d6f363f3ee
horohoro-Git <horohoro990420@gmail.com>
2025-05-08 03:53:20 +0900

    Adding emotional expressions to animals

--- Assets/scripts/newScripts/FPS.cs
+++ Assets/scripts/newScripts/FPS.cs
@@ -23,10 +23,7 @@ public class FPS : MonoBehaviour
         this.width = Screen.width;
         this.height = Screen.height;
         this.style = new GUIStyle();
-       //   Font font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-   //     this.style.font = font;
-        //UnityEngine.Profiling.Profiler.logFile = "mylog.log";
-    //    UnityEngine.Profiling.Profiler.enabled = true;
+     
     }
   //  private float lastGCAlloc = 0f;
  //   private float lastFrameTime = 0f;
@@ -34,44 +31,20 @@ public class FPS : MonoBehaviour
     WaitForSeconds tenSecnonds = new WaitForSeconds(10);
     private void Start()
     {
-       // Resources.UnloadUnusedAssets();
-        // AssetBundle.UnloadAllAssetBundles(true);
-        //Resources.UnloadAll();
-        //  StartCoroutine(ResetLowFPS());
+     
     }
 
-    IEnumerator ResetLowFPS()
-    {
-
-        while (true)
-        {
-            lowFPS = 0.0f;
-            yield return tenSecnonds;
-        }
-    }
     float lastRecordedTime = 0f;
  //   float sampleInterval = 1f;
     void Update()
     {
         this.deltaTime += (Time.deltaTime - this.deltaTime) * 0.1f;
-        /*  this.deltaTime += (Time.deltaTime - this.deltaTime) * 0.1f;
-
-          this.totalReservedMemory = UnityEngine.Profiling.Profiler.GetTotalReservedMemoryLong();
-          this.totalAllocMemory = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong();
-          this.totalUnusedReservedMemory = UnityEngine.Profiling.Profiler.GetTotalUnusedReservedMemoryLong();*/
-
-        // 매 프레임마다 메모리 상태 확인 (로그 출력)
-        /*Debug.Log($"Allocated Memory: {Profiler.GetTotalAllocatedMemoryLong() / 1024f / 1024f} MB");
-        Debug.Log($"Reserved Memory: {Profiler.GetTotalReservedMemoryLong() / 1024f / 1024f} MB");*/
+      
         lastRecordedTime += Time.deltaTime;
         if (Application.isPlaying && lastRecordedTime > 1)
         {
-         //   Resources.UnloadUnusedAssets();
             lastRecordedTime = 0;
-            // 실제 게임 실행 중에만 할당된 메모리 추적
-         //   Debug.Log($"Allocated Memory: {Profiler.GetTotalAllocatedMemoryLong() / 1024f / 1024f} MB");
-           // Debug.Log($"Allocated Memory: {Profiler.GetTotalAllocatedMemoryLong() / 1024f / 1024f} MB");
-         //   Debug.Log($"Reserved Memory (Play Mode): {Profiler.GetTotalReservedMemoryLong() / 1024f / 1024f} MB");
+           
         }
     }
 
@@ -85,14 +58,23 @@ public class FPS : MonoBehaviour
 
         // 텍스트 박스 설정
         Rect rect = new Rect(400, 0, width, height * 2 / 100);
+        Rect rect2 = new Rect(400, 200, width, height * 2 / 100);
         this.style.alignment = TextAnchor.UpperLeft;
         this.style.fontSize = this.height * 4 / 100;
         this.style.normal.textColor = Color.yellow;
+
+
+        float m = Profiler.GetTotalAllocatedMemoryLong() / (1024f * 1024f);
         // 텍스트 생성
         string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-
+        string memoryText = string.Format(
+         "Memory: {0:0.00} MB\nSystem: {1:0.00} MB",
+         Profiler.GetTotalAllocatedMemoryLong() / (1024f * 1024f),
+         System.GC.GetTotalMemory(false) / (1024f * 1024f)
+     );
         // 텍스트 표시
         GUI.Label(rect, text, style);
+        GUI.Label(rect2, memoryText, style);
         /*   Rect rect = new Rect(400, 0, this.width, this.height * 2 / 100);
            this.style.alignment = TextAnchor.UpperLeft;
            this.style.fontSize = this.height * 4 / 100;
